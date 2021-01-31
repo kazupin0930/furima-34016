5263e3c53c502854440f8b66ba5e83ff6769bff0
kazupin0930 <jurkt.kantaros@icloud.com>
2021-01-31 16:33:00 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_065825) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "shipping_charges_id", null: false
-    t.integer "shipping_area_id", null: false
-    t.integer "day_to_ship_id", null: false
-    t.integer "price", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "purchase_managements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_purchase_managements_on_item_id"
-    t.index ["user_id"], name: "index_purchase_managements_on_user_id"
-  end
-
-  create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postal_code", null: false
-    t.integer "shipping_area_id", null: false
-    t.string "municipality", null: false
-    t.string "address", null: false
-    t.string "building_name"
-    t.string "phone_number", null: false
-    t.bigint "purchase_management_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["purchase_management_id"], name: "index_shipping_addresses_on_purchase_management_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_frigana", null: false
-    t.string "first_name_frigana", null: false
-    t.date "date_of_birth", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "items", "users"
-  add_foreign_key "purchase_managements", "items"
-  add_foreign_key "purchase_managements", "users"
-  add_foreign_key "shipping_addresses", "purchase_managements"
 end
