59afbaa0cdcbe87b05d6d4b4d741b3de00f3084a
kazupin0930 <jurkt.kantaros@icloud.com>
2021-02-11 19:06:45 +0900

    テーブルに保存処理

--- app/models/purchase_management_shipping_address.rb
+++ app/models/purchase_management_shipping_address.rb
@@ -12,6 +12,7 @@ class PurchaseManagementShippingAddress < ApplicationRecord
   validates :shipping_area_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-
+    purchase_management = Purchase_management.create(item_id: item_id, user_id: user_id)
+    Shipping.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_management_id: purchase_managemet_id)
   end
 end
