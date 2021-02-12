00738e4e0ecd8909625560c026609a0fad45dc39
kazupin0930 <jurkt.kantaros@icloud.com>
2021-02-12 16:24:20 +0900

    orderコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
-    @item = Purchase_management_shipping_address.new
-  end
-
-  def new
+    @order = PurchaseManagementShippingAddress.new
+    @item = PurchaseManagementShippingAddress.new
   end
 
   def create
+    @purchase_management_shippingaddress = PurchaseManagementShippingAddress.new(purchase_management_params)
+    if purchase_management_shippingaddress.valid?
+      @purchase_management_shippingaddress.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def purchase_management_params
+    params.require(:purchase_management_shippingaddress).permit(:postal_code, :shipping_area, :municipality, :address, :builing_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
--- app/models/purchase_management_shipping_address.rb
+++ app/models/purchase_management_shipping_address.rb
@@ -1,7 +1,7 @@
-class PurchaseManagementShippingAddress < ApplicationRecord
+class PurchaseManagementShippingAddress
 
   include ActiveModel: :Model
-  attr_accessor :postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :purchase_management_id
+  attr_accessor :postal_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :purchase_management_id, :item_id, :user_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "can't be blank. Input correctly" } 
@@ -12,7 +12,7 @@ class PurchaseManagementShippingAddress < ApplicationRecord
   validates :shipping_area_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-    purchase_management = Purchase_management.create(item_id: item_id, user_id: user_id)
-    Shipping.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_management_id: purchase_managemet_id)
+    purchase_management = PurchaseManagement.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_management_id: purchase_managemet_id)
   end
 end
