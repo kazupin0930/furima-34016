1c5ad9350141c4f62d24a5fa71d5b9f406c6ca3b
kazupin0930 <jurkt.kantaros@icloud.com>
2021-02-14 23:30:10 +0900

    テストコード仮

--- spec/factories/purchase_management_shipping_addresses.rb
+++ spec/factories/purchase_management_shipping_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase_management_shipping_address do
-    
+    postal_code { '123-0001' }
+    shipping_area_id { '2' }
+    municipality { '横浜市' }
+    address { '青山1-1' }
+    building_name { '青山ビル' }
+    phone_number { '09012341234' }
+
   end
 end
--- spec/models/purchase_management_shipping_address_spec.rb
+++ spec/models/purchase_management_shipping_address_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseManagementShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item)
+      @order = FactoryBot.create(:purchase_management_shipping_address, user_id: @user.id, item_id: @item.id)
+    end
+
+    describe '商品購入機能' do
+      context '購入できるとき' do
+        it '全て入力されていれば購入できること' do
+          expect(@order).to be_valid
+        end
+
+        it '建物名がnilでも購入できること' do
+          @order.building_name = ""
+          expect(@order).to be_valid
+        end
+      end
+
+      context '商品購入できないとき' do
+        it '配送先の情報として、郵便番号が無いと購入できないこと' do
+          @order.postal_code = ""
+          @order.valid?
+          expect(@order.error.full_messages).to include("Postal code can't be blank")
+        end
+
+        it '配送先の情報として、都道府県の値が1だと購入できないこと' do
+          @order.shipping_area_id = "1"
+          @order.valid?
+          expect(@order.error.full_messages).to include('Shipping area Select')
+        end
+
+        it '配送先の情報として、市区町村が無いと購入できないこと' do
+          @order.municipality = ""
+          @order.valid?
+          expect(@order.error.full_messages).to include("Municipality can't be blank")
+        end
+
+        it '配送先の情報として、番地が無いと購入できないこと' do
+          @order.address = ""
+          @order.valid?
+          expect(@order.error.full_messages).to include("Address can't be blank")
+        end
+
+        it '配送先の情報として、電話番号が無いと購入できないこと' do
+          @order.phone_number = ""
+          @order.valid?
+          expect(@order.error.full_messages).to include("Phone number can't be blank")
+        end
+
+        it '郵便番号の保存にはハイフンが無いと購入できないこと（123-4567となる）' do
+          @order.postal_code = "1231111"
+          @order.valid?
+          expect(@order.error.full_messages).to include("Postal code Input correctly")
+        end
+
+        it '電話番号は11桁以内の数値のみ保存可能なこと（09012345678となる）' do
+          @order.phone_number = "aaaaaaaaaaa"
+          @order.valid?
+          expect(@order.error.full_messages).to include("Phone number Input correctly")
+        end
+      end
+    end
+  end
 end
